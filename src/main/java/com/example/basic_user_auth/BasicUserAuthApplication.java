05d88b1fb64ea36eef658a75aa94baa0f4cea38a
Charles Kamiri <charleskamiri6@gmail.com>
2023-02-20 13:23:33 +0300

    first commit

---
+++ src/main/java/com/example/basic_user_auth/BasicUserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic_user_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicUserAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicUserAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basic_user_auth/BasicUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_user_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
